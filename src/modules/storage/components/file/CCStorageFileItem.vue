117d72e7b81b8ecc0b24d4e4d2ad2b226911fbe5
Ilvina <elbuuur@gmail.com>
2023-09-13 13:30:02 +0300

    add incons in entity items components

--- src/modules/storage/components/file/CCStorageFileItem.vue
+++ src/modules/storage/components/file/CCStorageFileItem.vue
@@ -4,6 +4,7 @@
       class="max-h-4 mt-0.5"
       :file-type="file.extension"
     ></file-type-icon>
+
     <div class="px-3 mr-auto flex flex-col overflow-x-hidden">
       <div class="text-sm text-gray-800 single-line-ellipsis">
         {{ file.file_name }}
@@ -12,17 +13,7 @@
 
     <div data-entity-menu class="relative z-10">
       <div @click="isOpenMenu = !isOpenMenu" class="cursor-pointer">
-        <svg
-          fill="currentColor"
-          class="w-5 h-5"
-          style=""
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path
-            d="M10 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0-6a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-          ></path>
-        </svg>
+        <menu-action-icon></menu-action-icon>
       </div>
 
       <div
@@ -33,71 +24,30 @@
           @click.stop="openFileNameChangeModal(file.id)"
           text="Rename"
         >
-          <svg viewBox="0 0 20 20" fill="currentColor">
-            <path
-              d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
-            ></path>
-          </svg>
+          <rename-action-icon></rename-action-icon>
         </popup-action-button>
 
         <popup-action-button
           @click.stop="openFileShelfLifeChangeModal(file.id)"
           text="Change retention days"
         >
-          <svg viewBox="0 0 20 20" fill="currentColor">
-            <path
-              fill-rule="evenodd"
-              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
+          <change-date-action-icon></change-date-action-icon>
         </popup-action-button>
 
         <popup-action-button text="Move to...">
-          <svg viewBox="0 0 20 20" fill="currentColor">
-            <path
-              fill-rule="evenodd"
-              d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
-              clip-rule="evenodd"
-            ></path>
-            <path
-              d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z"
-            ></path>
-          </svg>
+          <move-file-action-icon></move-file-action-icon>
         </popup-action-button>
 
         <hr class="m-0.5" />
 
         <popup-action-button text="Download">
-          <svg viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5" style="">
-            <path
-              fill-rule="evenodd"
-              d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
+          <download-action-icon></download-action-icon>
         </popup-action-button>
 
         <hr class="m-0.5" />
 
         <popup-action-button text="Info">
-          <svg
-            width="20px"
-            height="20px"
-            viewBox="0 0 48 48"
-            fill="currentColor"
-          >
-            <g id="Layer_2" data-name="Layer 2">
-              <g id="invisible_box" data-name="invisible box">
-                <rect width="48" height="48" fill="none" />
-              </g>
-              <g id="icons_Q2" data-name="icons Q2">
-                <path
-                  d="M24,2A22,22,0,1,0,46,24,21.9,21.9,0,0,0,24,2Zm2,32a2,2,0,0,1-4,0V22a2,2,0,0,1,4,0ZM24,16a2,2,0,1,1,2-2A2,2,0,0,1,24,16Z"
-                />
-              </g>
-            </g>
-          </svg>
+          <info-action-icon></info-action-icon>
         </popup-action-button>
 
         <hr class="m-0.5" />
@@ -106,13 +56,7 @@
           @click.stop="moveFileToTrash(file.id)"
           text="Move to trash"
         >
-          <svg viewBox="0 0 20 20" fill="currentColor">
-            <path
-              fill-rule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
+          <move-to-trash-action-icon></move-to-trash-action-icon>
         </popup-action-button>
       </div>
     </div>
@@ -136,10 +80,19 @@
 
 <script setup lang="ts">
 import { ref, defineProps, defineEmits } from "vue";
+
 import { useEventListener } from "@/composables/useEventListener";
 
-import FileTypeIcon from "@/components/kit/icon/FileTypeIcon.vue";
 import PopupActionButton from "@/components/kit/button/PopupActionButton.vue";
+
+import FileTypeIcon from "@/components/kit/icon/FileTypeIcon.vue";
+import RenameActionIcon from "@/components/kit/icon/actions/RenameActionIcon.vue";
+import InfoActionIcon from "@/components/kit/icon/actions/InfoActionIcon.vue";
+import MoveToTrashActionIcon from "@/components/kit/icon/actions/MoveToTrashActionIcon.vue";
+import MenuActionIcon from "@/components/kit/icon/actions/MenuActionIcon.vue";
+import ChangeDateActionIcon from "@/components/kit/icon/actions/ChangeDateActionIcon.vue";
+import MoveFileActionIcon from "@/components/kit/icon/actions/MoveFileActionIcon.vue";
+import DownloadActionIcon from "@/components/kit/icon/actions/DownloadActionIcon.vue";
 
 type FileProps = {
   file: Record<string, string>;
--- src/modules/storage/components/folder/CCStorageFolderItem.vue
+++ src/modules/storage/components/folder/CCStorageFolderItem.vue
@@ -1,44 +1,11 @@
 <template>
-  <svg
-    class="svg-icon-size"
-    width="24"
-    height="24"
-    viewBox="0 0 1024 1024"
-    fill="#000000"
-  >
-    <g id="SVGRepo_bgCarrier" stroke-width="0" />
-    <g
-      id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <g id="SVGRepo_iconCarrier">
-      <path
-        d="M853.333333 256H469.333333l-85.333333-85.333333H170.666667c-46.933333 0-85.333333 38.4-85.333334 85.333333v170.666667h853.333334v-85.333334c0-46.933333-38.4-85.333333-85.333334-85.333333z"
-        fill="#FFA000"
-      />
-      <path
-        d="M853.333333 256H170.666667c-46.933333 0-85.333333 38.4-85.333334 85.333333v426.666667c0 46.933333 38.4 85.333333 85.333334 85.333333h682.666666c46.933333 0 85.333333-38.4 85.333334-85.333333V341.333333c0-46.933333-38.4-85.333333-85.333334-85.333333z"
-        fill="#FFCA28"
-      />
-    </g>
-  </svg>
+  <folder-type-icon></folder-type-icon>
   <div class="px-3 mr-auto text-sm text-gray-800 single-line-ellipsis">
     {{ folder.folder_name }}
   </div>
   <div data-entity-menu class="relative">
     <div @click="isOpenMenu = !isOpenMenu" class="cursor-pointer">
-      <svg
-        fill="currentColor"
-        class="w-5 h-5"
-        style=""
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <path
-          d="M10 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0-6a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-        ></path>
-      </svg>
+      <menu-action-icon></menu-action-icon>
     </div>
 
     <div
@@ -46,28 +13,13 @@
       class="mt-1 absolute bg-white text-gray-600 origin-top-right right-0 mt-2 w-56 shadow-lg overflow-hidden"
     >
       <popup-action-button @click.stop="openFolderChangeModal()" text="Rename">
-        <svg viewBox="0 0 20 20" fill="currentColor">
-          <path
-            d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
-          ></path>
-        </svg>
+        <rename-action-icon></rename-action-icon>
       </popup-action-button>
 
       <hr class="m-0.5" />
 
       <popup-action-button text="Info">
-        <svg width="20px" height="20px" viewBox="0 0 48 48" fill="currentColor">
-          <g id="Layer_2" data-name="Layer 2">
-            <g id="invisible_box" data-name="invisible box">
-              <rect width="48" height="48" fill="none" />
-            </g>
-            <g id="icons_Q2" data-name="icons Q2">
-              <path
-                d="M24,2A22,22,0,1,0,46,24,21.9,21.9,0,0,0,24,2Zm2,32a2,2,0,0,1-4,0V22a2,2,0,0,1,4,0ZM24,16a2,2,0,1,1,2-2A2,2,0,0,1,24,16Z"
-              />
-            </g>
-          </g>
-        </svg>
+        <info-action-icon></info-action-icon>
       </popup-action-button>
 
       <hr class="m-0.5" />
@@ -75,13 +27,7 @@
         @click.stop="moveFolderToTrash()"
         text="Move to trash"
       >
-        <svg viewBox="0 0 20 20" fill="currentColor">
-          <path
-            fill-rule="evenodd"
-            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+        <move-to-trash-action-icon></move-to-trash-action-icon>
       </popup-action-button>
     </div>
   </div>
@@ -96,13 +42,19 @@
 
 <script setup lang="ts">
 import { ref, defineProps, defineEmits } from "vue";
+
 import { useEventListener } from "@/composables/useEventListener";
+import { httpClient } from "@/api";
+import { useFlashNotificationStore } from "@/stores/flashNotifications";
 
 import PopupActionButton from "@/components/kit/button/PopupActionButton.vue";
 import CCStorageFolderItemChangeNameForm from "@/modules/storage/components/folder/CCStorageFolderItemChangeNameForm.vue";
-import { httpClient } from "@/api";
 
-import { useFlashNotificationStore } from "@/stores/flashNotifications";
+import FolderTypeIcon from "@/components/kit/icon/FolderTypeIcon.vue";
+import RenameActionIcon from "@/components/kit/icon/actions/RenameActionIcon.vue";
+import InfoActionIcon from "@/components/kit/icon/actions/InfoActionIcon.vue";
+import MoveToTrashActionIcon from "@/components/kit/icon/actions/MoveToTrashActionIcon.vue";
+import MenuActionIcon from "@/components/kit/icon/actions/MenuActionIcon.vue";
 
 type FolderProps = {
   folder: Record<string, string | number>;
